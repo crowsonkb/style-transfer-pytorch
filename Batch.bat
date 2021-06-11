7e222de21cde9f78cdb9572ebe1a58338749a1f6
xpsychonauticon <54873330+xpscyho@users.noreply.github.com>
2021-06-11 10:44:58 -0400

    Update Batch.bat
    recent merge broke the code, my mistake lmao

--- Batch.bat
+++ Batch.bat
@@ -1,7 +1,7 @@
 
 @echo off
 echo Style transfer Quick CLI tool
-goto a
+
 setlocal enabledelayedexpansion enableextensions
 set /p Input="Input Path:"
 echo Style Path(I would advise using less than 10 style images, i'm not sure how many the AI can input)
@@ -15,7 +15,7 @@ set /p Output="Output Path:"
 style_transfer -h
 set /p Additionals="Additional Settings, Use Default if you're not sure what to do:" || set Additionals=--web --host localhost --browser
   ::--web --host localhost  -ms 64 -s 128 -cw 0.05 -ss 0.1  --browser -i 500 -ii 1000 --save-every 75 -tw 3 (my favorite)
-:a
+
 cd /d %Input%
 set InputShort=%Input:~-15%
 set ListShort=%LIST:~-15%

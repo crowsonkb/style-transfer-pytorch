6e145def6a3b6fad85f853d8c6f922c74ca31dbf
xpsychonauticon <54873330+xpscyho@users.noreply.github.com>
2021-06-11 10:35:16 -0400

    Update Batch.bat and create test branch
    batch improvements

--- Batch.bat
+++ Batch.bat
@@ -1,7 +1,7 @@
 
 @echo off
 echo Style transfer Quick CLI tool
-
+goto a
 setlocal enabledelayedexpansion enableextensions
 set /p Input="Input Path:"
 echo Style Path(I would advise using less than 10 style images, i'm not sure how many the AI can input)
@@ -15,12 +15,18 @@ set /p Output="Output Path:"
 style_transfer -h
 set /p Additionals="Additional Settings, Use Default if you're not sure what to do:" || set Additionals=--web --host localhost --browser
   ::--web --host localhost  -ms 64 -s 128 -cw 0.05 -ss 0.1  --browser -i 500 -ii 1000 --save-every 75 -tw 3 (my favorite)
-
+:a
 cd /d %Input%
-echo Input       %Input%
-echo "Style(s)"  %LIST%
-echo Output      %Output%
-echo Additionals %Additionals%
+set InputShort=%Input:~-15%
+set ListShort=%LIST:~-15%
+set OutputShort=%Output:~-15%
+set AddShort=%Additionals:~-15%
+echo "Input"       %InputShort%
+echo "Style(s)"    %ListShort%
+echo "Output"      %OutputShort%
+echo "Additionals" %AddShort%
+
+
 for %%i in (*) do (
 echo %%i
 style_transfer %Input%\%%i %LIST% -o %Output%\%%i %Additionals%%Browser%

a60d166461c7c1db4f6ab77582bac50bb2413484
xpsychonauticon <54873330+xpscyho@users.noreply.github.com>
2021-06-11 10:02:46 -0400

    Rephrasing and cleanup

--- Batch.bat
+++ Batch.bat
@@ -4,16 +4,14 @@ echo Style transfer Quick CLI tool
 
 setlocal enabledelayedexpansion enableextensions
 set /p Input="Input Path:"
-
-set /p Style="Style Path(paste single or multiple images to folder):"
+echo Style Path(I would advise using less than 10 style images, i'm not sure how many the AI can input)
+set /p Style="(Style folder path):"
 set STYLES=
 for %%x in (%Style%\*) do set LIST=!LIST! %%x
 set LIST=%LIST:~1%
 echo %LIST%
 set /p Output="Output Path:"
 
-
-:NOBROWSE
 style_transfer -h
 set /p Additionals="Additional Settings, Use Default if you're not sure what to do:" || set Additionals=--web --host localhost --browser
   ::--web --host localhost  -ms 64 -s 128 -cw 0.05 -ss 0.1  --browser -i 500 -ii 1000 --save-every 75 -tw 3 (my favorite)
